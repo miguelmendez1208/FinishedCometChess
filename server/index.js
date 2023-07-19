a3efc4c2d56652d821506abd4c3d758c5b5cfbdc
miguelmendez1208 <miguelmendez1208@gmail.com>
2023-07-19 11:19:08 -0500

    I have no idea what I'm doing

--- server/index.js
+++ server/index.js
@@ -6,10 +6,24 @@ const io = require("socket.io")(httpServer, {
 });
 
 io.use((socket, next) => {
+  const sessionID = socket.handshake.auth.sessionID;
+  if (sessionID) {
+    // find existing session
+    const session = sessionStore.findSession(sessionID);
+    if (session) {
+      socket.sessionID = sessionID;
+      socket.userID = session.userID;
+      socket.username = session.username;
+      return next();
+    }
+  }
   const username = socket.handshake.auth.username;
   if (!username) {
     return next(new Error("invalid username"));
   }
+  // create new session
+  socket.sessionID = randomId();
+  socket.userID = randomId();
   socket.username = username;
   next();
 });
@@ -17,6 +31,9 @@ io.use((socket, next) => {
 io.on("connection", (socket) => {
   // fetch existing users
   const users = [];
+  socket.join(socket.userID);
+
+
   for (let [id, socket] of io.of("/").sockets) {
     users.push({
       userID: id,
@@ -33,23 +50,40 @@ io.on("connection", (socket) => {
 
   // forward the private message to the right recipient
   socket.on("private message", ({ content, to }) => {
-    socket.to(to).emit("private message", {
+    socket.to(to).to(socket.userID).emit("private message", {
       content,
-      from: socket.id,
+      from: socket.userID,
+      to,
     });
   });
 
+  socket.emit("session", {
+    sessionID: socket.sessionID,
+    userID: socket.userID,
+  });
+  
   //forward the chess move
-  socket.on("chess move", ({ content, to}) => {
-    socket.to(to).emit("chess move", {
+  socket.on("chess move", ({ content, to }) => {
+    socket.to(to).to(socket.userID).emit("chess move", {
       content,
-      from: socket.id,
+      from: socket.userID,
+      to,
     });
   });
 
-  // notify users upon disconnection
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("user disconnected", socket.id);
+  socket.on("disconnect", async () => {
+    const matchingSockets = await io.in(socket.userID).allSockets();
+    const isDisconnected = matchingSockets.size === 0;
+    if (isDisconnected) {
+      // notify other users
+      socket.broadcast.emit("user disconnected", socket.userID);
+      // update the connection status of the session
+      sessionStore.saveSession(socket.sessionID, {
+        userID: socket.userID,
+        username: socket.username,
+        connected: false,
+      });
+    }
   });
 });
 
---
+++ server/sessionStore.js
@@ -0,0 +1,20 @@
+class InMemorySessionStore extends SessionStore {
+    constructor() {
+      super();
+      this.sessions = new Map();
+    }
+  
+    findSession(id) {
+      return this.sessions.get(id);
+    }
+  
+    saveSession(id, session) {
+      this.sessions.set(id, session);
+    }
+  
+    findAllSessions() {
+      return [...this.sessions.values()];
+    }
+  }
+  
+  
